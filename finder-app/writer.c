f0e9142aee77e4a5c00076b916f9bf54aff20901
Vihar Vasavada <vvasavada605@gmail.com>
2025-11-16 18:30:09 -0500

    Clean up

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,31 +1,23 @@
+// Writer program for assignment 2
+// Author: Vihar Vasavada
+
 #include <string.h>
 #include <stdio.h>
 #include <syslog.h>
 
-// One difference from the write.sh instructions in Assignment 1:  You do not need to make your "writer" utility create directories which do not exist.  You can assume the directory is created by the caller.
-
-// Setup syslog logging for your utility using the LOG_USER facility.
-
-// Use the syslog capability to write a message “Writing <string> to <file>” where <string> is the text string written to file (second argument) and <file> is the file created by the script.  This should be written with LOG_DEBUG level.
-
-// Use the syslog capability to log any unexpected errors with LOG_ERR level.
-
 int main(int argc, char *argv[])
 {
-    // 1. Check if you have the right number of arguments
-    //    (what should that be?)
     openlog( "Writer", LOG_CONS, LOG_USER );
     if( argc != 3 )
     {
         syslog( LOG_ERR, "%s", "Incorrect arguments. Usage: ./writer <FILE_PATH> <TEXT>" );
         return 1;
     }
-    
+
     char *filePath = argv[1];
     char *fileText = argv[2];
     FILE *filePtr;
-    
-    // 3. Open/create the file for writing
+
     filePtr = fopen( filePath, "w" );
     if( filePtr == NULL )
     {
