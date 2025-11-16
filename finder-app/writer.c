2cb25392d61f83f1d3ec29b64d7544898c58957d
Vihar Vasavada <vvasavada605@gmail.com>
2025-11-16 18:23:44 -0500

    Fixed error log

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     filePtr = fopen( filePath, "w" );
     if( filePtr == NULL )
     {
-        syslog( LOG_ERR, "Error opening file %s", " filePath" );
+        syslog( LOG_ERR, "Error opening file %s", filePath );
         return 1;
     }
 
