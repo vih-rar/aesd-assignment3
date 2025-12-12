86609db2a60dc7cc7d5502ea64781810630a06ee
Vihar Vasavada <vvasavada605@gmail.com>
2025-12-12 13:26:57 -0500

    Clean up

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -23,9 +23,14 @@ fi
 
 ORIG_PWD=$(pwd)
 mkdir -p ${OUTDIR}
-OUTDIR=$(pwd)/$OUTDIR
+
+if [[ "${OUTDIR}" != /* ]]; then
+    OUTDIR=$ORIG_PWD/${OUTDIR}
+fi
+
 echo $OUTDIR
 cd "$OUTDIR"
+
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
@@ -36,15 +41,12 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
     echo "clean done"
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
     echo "defconf done"
     make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
     echo "all done"
-    # make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
-    # echo "module done"
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
@@ -52,34 +54,34 @@ echo "Adding the Image in outdir"
 cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image $OUTDIR
 
 echo "Creating the staging directory for the root filesystem"
+
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
+mkdir -p $OUTDIR/rootfs
+ROOTFS=${OUTDIR}/rootfs
 
-# TODO: Create necessary base directories
-mkdir -p $OUTDIR/rootfs 
-cd $OUTDIR/rootfs
+cd $ROOTFS
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p usr/bin usr/lib usr/sbin usr/lib64
 mkdir -p var/log
 
-cd "$OUTDIR"
+cd $OUTDIR
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
 make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
 make CONFIG_PREFIX=${OUTDIR}/busybox ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
 
@@ -89,20 +91,17 @@ BUSYBOX_BIN=${OUTDIR}/busybox/bin/busybox
 
 ${CROSS_COMPILE}readelf -l "${BUSYBOX_BIN}" | awk '/Interpreter|Shared library/ {print}'
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-ROOTFS=${OUTDIR}/rootfs
-
-sudo mkdir -p "${ROOTFS}/lib" "${ROOTFS}/lib64" "${ROOTFS}/usr/lib" "${ROOTFS}/usr/lib64"
 
 INTERP=$(${CROSS_COMPILE}readelf -l "${BUSYBOX_BIN}" | grep 'program interpreter' | awk -F': ' '/program interpreter/ {gsub(/[][]/,"",$2); print $2}')
 
-SRC="${SYSROOT}${INTERP}"
-sudo cp $SRC $ROOTFS/lib/
+INTERO_SRC="${SYSROOT}${INTERP}"
+sudo cp $INTERO_SRC $ROOTFS/lib/
 
-LIB=$(${CROSS_COMPILE}readelf -a "${BUSYBOX_BIN}" | grep 'Shared library'  \
+SHARED_LIBS=$(${CROSS_COMPILE}readelf -a "${BUSYBOX_BIN}" | grep 'Shared library'  \
   | awk -F'[][]' '{print $2}' \
   | sort -u )
 
-for lib in $LIB; do
+for lib in $SHARED_LIBS; do
     FOUND=$(find $SYSROOT -name $lib)
     sudo cp $FOUND $ROOTFS/lib64/
 done
@@ -112,23 +111,17 @@ sudo cp -a ${OUTDIR}/busybox/* ${ROOTFS}/
 sudo mknod -m 666 "${ROOTFS}/dev/null" c 1 3
 sudo mknod -m 600 "${ROOTFS}/dev/console" c 5 1
 
-echo "Done"
-# TODO: Clean and build the writer utility
+echo "RootFS Done"
+
 cd $ORIG_PWD
 make clean
 make CROSS_COMPILE=$CROSS_COMPILE
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
-cp finder.sh writer $ROOTFS/home
-cp finder-test.sh conf/username.txt conf/assignment.txt $ROOTFS/home
-cp autorun-qemu.sh $ROOTFS/home
-
-# TODO: Chown the root directory
+sudo cp finder-test.sh finder.sh writer autorun-qemu.sh $ROOTFS/home
+sudo cp -rL conf $ROOTFS/home/
 
 sudo chown -R root:root "${ROOTFS}"
 
-# TODO: Create initramfs.cpio.gz
 cd $ROOTFS
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 gzip -f ${OUTDIR}/initramfs.cpio
